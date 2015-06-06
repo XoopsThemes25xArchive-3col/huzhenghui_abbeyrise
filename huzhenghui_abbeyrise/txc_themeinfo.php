dbd42c2914cf7482263c9a6643128ba887ae855b
mambax7 <mambax7@gmail.com>
2015-06-06 07:23:59 -0400

    Original files

---
+++ huzhenghui_abbeyrise/txc_themeinfo.php
@@ -0,0 +1,7 @@
+<?php
+	$txc_themeinfo["name"] = "huzhenghui_abbeyrise";
+	$txc_themeinfo["author"] = "ºúÕù»Ô";
+	$txc_themeinfo["author_url"] = "http://xoops.org.cn/userinfo.php?uid=8616";
+	$txc_themeinfo["detail_url"] = "http://xoops.org.cn/modules/newbb/viewtopic.php?topic_id=11501";
+	$txc_themeinfo["download_url"] = "http://xoops.org.cn/modules/newbb/viewtopic.php?topic_id=11501";
+?>
